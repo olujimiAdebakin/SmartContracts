747ab81d0d4963e441f0563cf68154014a4f2ce7
olujimiAdebakin <omoladebu231@gmail.com>
2025-04-09 16:44:02 +0100

    updated

---
+++ contracts/eventsTicket.sol
@@ -0,0 +1,22 @@
+
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.18;
+
+// EVENT TICKET CONTRACT
+contract EventTicket{
+    
+    // STATE VARIABLES
+
+    uint256 public numberOfTickets;
+    uint256 public ticketPrice;
+    uint256 public totalNumberOfTicket;
+    uint256 public StartAt;
+    uint256 public EndAt;
+    uint256 public ticketstamp;
+    uint256 public ticketPricePerPerson;
+    string public message = "Buy Your First Event Ticket";
+
+
+}
